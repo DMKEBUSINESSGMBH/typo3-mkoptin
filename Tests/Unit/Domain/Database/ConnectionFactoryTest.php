cc98faa90ba9f5ef7cfd9be1dfd07ed063c4b4d4
Michael Wagner <michael.wagner@dmk-ebusiness.com>
2021-12-08 10:01:34 +0100

    Add ConnectionFactoryTest

---
+++ Tests/Unit/Domain/Database/ConnectionFactoryTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * Copyright notice
+ *
+ * (c) 2021 DMK E-BUSINESS GmbH <dev@dmk-ebusiness.de>
+ * All rights reserved
+ *
+ * This file is part of the "mkoptin" Extension for TYPO3 CMS.
+ *
+ * This script is part of the TYPO3 project. The TYPO3 project is
+ * free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * The GNU General Public License can be found at
+ * www.gnu.org/copyleft/gpl.html
+ *
+ * This script is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This copyright notice MUST APPEAR in all copies of the script!
+ */
+
+namespace DMK\Optin\Tests\Domain\Manager;
+
+use DMK\Optin\Domain\Database\ConnectionFactory;
+use Nimut\TestingFramework\TestCase\AbstractTestCase as NimutTestingFrameworkTestCase;
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * ConnectionFactory test.
+ *
+ * @author Michael Wagner
+ */
+class ConnectionFactoryTest extends NimutTestingFrameworkTestCase
+{
+    protected function tearDown()
+    {
+        GeneralUtility::purgeInstances();
+    }
+
+    /**
+     * @test
+     */
+    public function getDefaultConnection()
+    {
+        $connection = $this->prophesize(Connection::class);
+        $connectionPool = $this->prophesize(ConnectionPool::class);
+        GeneralUtility::addInstance(ConnectionPool::class, $connectionPool->reveal());
+        $connectionPool
+            ->getConnectionByName(ConnectionPool::DEFAULT_CONNECTION_NAME)
+            ->willReturn($connection->reveal())
+            ->shouldBeCalledOnce();
+
+        $factory = new ConnectionFactory();
+        $this->assertSame(
+            $connection->reveal(),
+            $factory->getDefaultConnection()
+        );
+    }
+}
